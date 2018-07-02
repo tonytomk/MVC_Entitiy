269b9d29566f34c3184def152faa14185d7ba338
Tony Tom K <tony.tomk@thermofisher.com>
2018-07-02 22:49:09 +0530

    Initial commit

---
+++ MVC_Entity/MVC_Entity/Controllers/EmployeesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Entity.Models;
+
+namespace MVC_Entity.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employees = db.Employees.Include(e => e.Department);
+            return View(employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,Name,Gender,City,DepartmentId,DateOfBirth")] Employee employee)
+        {
+            if (string.IsNullOrEmpty(employee.Name))
+            {
+                ModelState.AddModelError("Name", "The Name field is required");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.departments = new SelectList(db.Departments, "Id", "Name");
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeId,Gender,City,DepartmentId,DateOfBirth")] Employee employee)
+        {
+            Employee employeeFromDB = db.Employees.Single(x => x.EmployeeId == employee.EmployeeId);
+
+            employeeFromDB.EmployeeId = employee.EmployeeId;
+            employeeFromDB.Gender = employee.Gender;
+            employeeFromDB.City = employee.City;
+            employeeFromDB.DepartmentId = employee.DepartmentId;
+            employee.Name = employeeFromDB.Name;
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeeFromDB).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       public ActionResult EmployeesByDepartment()
+        {
+            var departmentTotals = db.Employees.Include("Department")
+                .GroupBy(x => x.Department.Name)
+                .Select(y => new DepartmentTotals
+                {
+                    Name = y.Key,
+                    Total = y.Count()
+                }).ToList().OrderByDescending(y=>y.Total);
+            return View(departmentTotals);
+        }
+    }
+}
---
+++ MVC_Entity/MVC_Entity/Models/DepartmentTotals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity.Models
+{
+    public class DepartmentTotals
+    {
+        public string Name { get; set; }
+        public int Total { get; set; }
+    }
+}
